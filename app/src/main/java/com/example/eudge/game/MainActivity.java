74b0ce785aa1e3f8d89795fe186f2a37f6406d3b
EudgenT <eudgentt@gmail.com>
2017-08-28 10:02:53 +0300

    fix butterknife

--- app/src/main/java/com/example/eudge/game/MainActivity.java
+++ app/src/main/java/com/example/eudge/game/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.eudge.game;
 
+import android.graphics.Color;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,34 +15,68 @@ import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @BindView(R.id.radioGroup)
-//    protected RadioGroup mRadioGroup;
-//    @BindView(R.id.radioButton1)
-//    protected RadioButton mRadioButton1;
-//    @BindView(R.id.radioButton2)
-//    protected RadioButton mRadioButton2;
-//    @BindView(R.id.radioButton3)
-//    protected RadioButton mRadioButton3;
-//    @BindView(R.id.textViewResult)
-//    protected TextView mTextViewResult;
-//    @BindView(R.id.checkButton) Button mCheckButton;
-//    @BindView(R.id.exitButton) Button mExitButton;
+    @BindView(R.id.radioGroup)
+    protected RadioGroup mRadioGroup;
+    @BindView(R.id.radioButton1)
+    protected RadioButton mRadioButton1;
+    @BindView(R.id.radioButton2)
+    protected RadioButton mRadioButton2;
+    @BindView(R.id.radioButton3)
+    protected RadioButton mRadioButton3;
+    @BindView(R.id.textViewResult)
+    protected TextView mTextViewResult;
 
-    Button mCheckButton;
-    Button mExitButton;
-    Button mClearButton;
-    RadioButton mRadioButton1;
-    RadioButton mRadioButton2;
-    RadioButton mRadioButton3;
-    RadioGroup mRadioGroup;
-    TextView mTextViewResult;
+    @OnClick(R.id.checkButton)
+    protected void testButton() {
+        if (mRadioGroup.getCheckedRadioButtonId() == -1)
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Нужно выбрать коробку!!!", Toast.LENGTH_SHORT).show();
+        else {
+            for (int i = 0; i < 3; i++) {
+                temp = (RadioButton) views[i];
+                if (temp.isChecked())
+                    checked = i;
+            }
+            for (int i = 0; i < 3; i++) {
+                if (mas[i] != 1 && checked != i) {
+                    temp = (RadioButton) views[i];
+                    temp.setEnabled(false);
+                    invisible = i;
+                    MainActivity.this.showAlert();
+                    temp = (RadioButton) views[position];
+                    temp.setTextColor(Color.RED);
+                    break;
+                }
+            }
+        }
+    }
+
+    @OnClick(R.id.buttonClear)
+    protected void clearButton() {
+        clean();
+        setRandomMas();
+    }
+
+    @OnClick(R.id.exitButton)
+    protected void exitButton() {
+        mTextViewResult.setText("Джексон: " + winDJ + " Мирополец: " + winMP);
+    }
+
+//    Button mCheckButton;
+//    Button mExitButton;
+//    Button mClearButton;
+//    RadioButton mRadioButton1;
+//    RadioButton mRadioButton2;
+//    RadioButton mRadioButton3;
+//    RadioGroup mRadioGroup;
+//    TextView mTextViewResult;
 
     final Random random = new Random();
     int[] mas = {0, 0, 0};
-    View[] views;
+    View[] views = new View[3];
     int position = 0;
     int checked = 0;
     int invisible = 0;
@@ -55,61 +90,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-        mCheckButton = (Button) findViewById(R.id.checkButton);
-        mExitButton = (Button) findViewById(R.id.exitButton);
-        mClearButton = (Button) findViewById(R.id.buttonClear);
+        views[0] = mRadioButton1;
+        views[1] = mRadioButton2;
+        views[2] = mRadioButton3;
 
-        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-        mRadioButton1 = (RadioButton) findViewById(R.id.radioButton1);
-        mRadioButton2 = (RadioButton) findViewById(R.id.radioButton2);
-        mRadioButton3 = (RadioButton) findViewById(R.id.radioButton3);
-        views = new View[]{mRadioButton1, mRadioButton2, mRadioButton3};
-
-        mTextViewResult = (TextView) findViewById(R.id.textViewResult);
+//        mCheckButton = (Button) findViewById(R.id.checkButton);
+//        mExitButton = (Button) findViewById(R.id.exitButton);
+//        mClearButton = (Button) findViewById(R.id.buttonClear);
+//
+//        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+//
+//        mRadioButton1 = (RadioButton) findViewById(R.id.radioButton1);
+//        mRadioButton2 = (RadioButton) findViewById(R.id.radioButton2);
+//        mRadioButton3 = (RadioButton) findViewById(R.id.radioButton3);
+//        views = new View[]{mRadioButton1, mRadioButton2, mRadioButton3};
+//
+//        mTextViewResult = (TextView) findViewById(R.id.textViewResult);
 
         setRandomMas();
 
-        mCheckButton.setOnClickListener(view -> {
-            if (mRadioGroup.getCheckedRadioButtonId() == -1)
-                Toast.makeText(MainActivity.this.getApplicationContext(), "Нужно выбрать коробку!!!", Toast.LENGTH_SHORT).show();
-            else {
-                for (int i = 0; i < 3; i++) {
-                    temp = (RadioButton) views[i];
-                    if (temp.isChecked())
-                        checked = i;
-                }
-                for (int i = 0; i < 3; i++) {
-                    if (mas[i] != 1 && checked != i) {
-                        temp = (RadioButton) views[i];
-                        temp.setEnabled(false);
-                        invisible = i;
-                        MainActivity.this.showAlert();
-                        break;
-                    }
-                }
-            }
-        });
-
-        mClearButton.setOnClickListener(view -> {
-            clean();
-            setRandomMas();
-        });
-
-        mExitButton.setOnClickListener( view -> {
-            mTextViewResult.setText("Джексон: " + winDJ + " Мирополец: " + winMP);
-        });
+//        mCheckButton.setOnClickListener(view -> {
+//
+//        });
+//
+//        mClearButton.setOnClickListener(view -> {
+//
+//        });
+//
+//        mExitButton.setOnClickListener(view -> );
     }
 
     protected void clean() {
-        mRadioButton1.setChecked(false);
-        mRadioButton2.setChecked(false);
-        mRadioButton3.setChecked(false);
-
-        for (int i = 0; i < 3; i++) {
+        for (int i=0;i<3;i++){
             temp = (RadioButton) views[i];
+            temp.setChecked(false);
+            temp.setTextColor(Color.BLACK);
             temp.setEnabled(true);
         }
     }
@@ -136,15 +153,16 @@ public class MainActivity extends AppCompatActivity {
                 if (checked != j && invisible != j) {
                     temp = (RadioButton) views[j];
                     temp.setChecked(true);
+                    position = j;
                     break;
                 }
             }
             if (position == checked) {
-                mTextViewResult.setText("Мирополец!");
-                winMP++;
-            } else {
                 mTextViewResult.setText("Джексон!");
                 winDJ++;
+            } else {
+                mTextViewResult.setText("Мирополец!");
+                winMP++;
             }
         });
         builder.setNegativeButton("No", (dialog, i) -> {
@@ -159,5 +177,4 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alert = builder.create();
         alert.show();
     }
-
 }
